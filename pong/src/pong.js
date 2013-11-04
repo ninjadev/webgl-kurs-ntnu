5f7aa8c7b6603c30a32db18075caa369c67b975e
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-11-04 01:12:47 +0100

    Fix error introduced by rebasing

--- pong/src/pong.js
+++ pong/src/pong.js
@@ -114,15 +114,15 @@ function setup(){
   var far = 10000;
 
   /* Create a THREEJS WebGL renderer. */
-  var renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer();
 
   /* Create a camera. This is the most useful camera type, acting like a real camera. */
-  var camera = new THREE.PerspectiveCamera(view_angle, aspect, near, far);
+  camera = new THREE.PerspectiveCamera(view_angle, aspect, near, far);
 
   /* Create a scene.
    * All lights and objects have to be placed here. 
    * Use scene.add(object); */
-  var scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
   /* Pull the camera back a little bit. Newly initialized cameras start at
    * (x: 0, y: 0, z: 0), so we need to pull the camera back so we can see
