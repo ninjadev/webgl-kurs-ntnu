76ff50c7092d798d36fe26e3cd9b99f0c4aebba2
Stian Jensen <me@stianj.com>
2013-11-04 03:13:26 +0100

    Add exercize one instructions

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var camera, scene, renderer;
-    var geometry, material, mesh;
+var currentDemo;
 
-    init();
-    animate();
-
-    function init() {
-
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-
-        scene = new THREE.Scene();
-
-        geometry = new THREE.CubeGeometry( 200, 200, 200 );
-        material = new THREE.MeshLambertMaterial({ color: 0xffa000 });
-
-        mesh = new THREE.Mesh( geometry, material );
-        mesh.position.z = -300;
-        scene.add( mesh );
-
-        scene.add(new THREE.DirectionalLight());
-        scene.add(new THREE.AmbientLight(0x444444));
-
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize( 400, 300);
-
-        document.getElementById('three-1').appendChild( renderer.domElement );
-
-    }
-
-    function animate() {
-
-        // note: three.js includes requestAnimationFrame shim
-        requestAnimationFrame( animate );
-
-        mesh.rotation.x += 0.01;
-        mesh.rotation.y += 0.02;
-
-        renderer.render( scene, camera );
-    }
+slideshow.on('afterShowSlide', function(slide) {
+  console.log(slide.properties);
+  switch (slide.properties.demo) {
+    case 'rotate-cube':
+      currentDemo = rotateCube();
+      break;
+  }
 });
+
+slideshow.on('hideSlide', function(slide) {
+  currentDemo && currentDemo.stop();
+});
+
+function rotateCube() {
+  var renderer = new THREE.WebGLRenderer();
+  var container = document.querySelector('#rotate-cube');
+  console.log(container.offsetWidth);
+  renderer.setSize(container.offsetWidth, container.offsetHeight);
+  container.appendChild(renderer.domElement);
+
+  var camera = new THREE.PerspectiveCamera(75, container.offsetWidth / container.offsetHeight, 1, 1000);
+  camera.position.z = 300;
+
+  var scene = new THREE.Scene();
+
+  var geometry = new THREE.CubeGeometry(200, 200, 200);
+  var material = new THREE.MeshLambertMaterial({
+      color: 0xff0000
+  });
+
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  var light = new THREE.PointLight(0xffffff);
+  light.position = new THREE.Vector3(0,100,400);
+  scene.add(light);
+
+  var pause = false;
+
+  function animate() {
+    !pause && requestAnimationFrame(animate);
+
+    mesh.rotation.x += .005;
+    mesh.rotation.y += .005;
+
+    renderer.render(scene, camera);
+  }
+  animate();
+  return {
+    stop: function() {
+      pause = true;
+      container.innerHTML = "";
+    }
+  };
+}
